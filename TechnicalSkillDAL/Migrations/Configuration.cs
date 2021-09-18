ea2f91a50275c931c50fa5ca2bfa03064d5b6a04
thuyetbn <thuyetbn96@gmail.com>
2021-09-18 20:03:56 +0700

    Update Done

--- TechnicalSkillDAL/Migrations/Configuration.cs
+++ TechnicalSkillDAL/Migrations/Configuration.cs
@@ -1,25 +1,16 @@
-﻿namespace TechnicalSkill.DAL.Migrations
-{
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+﻿
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
 
+namespace TechnicalSkill.DAL.Migrations
+{
     internal sealed class Configuration : DbMigrationsConfiguration<TechnicalSkill.DAL.DatabaseContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
-            AutomaticMigrationDataLossAllowed = true;
-            ContextKey = "Technical.DAL.DatabaseContext";
+            AutomaticMigrationsEnabled = false;
         }
-
-        protected override void Seed(TechnicalSkill.DAL.DatabaseContext context)
-        {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
-        }
-    }
+    } 
 }
